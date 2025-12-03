a468c4f153f6ecc4ab47a64ed763c3b94ef2a9a0
ekah Pruthvi <p1sribhat@gmail.com>
2025-12-03 19:05:09 +0530

    quote win2

--- business/script.js
+++ business/script.js
@@ -48,15 +48,42 @@ updateButtonText();
 
 const window1 = document.getElementById('window1');
 const window2 = document.getElementById('window2');
+const nameout = document.getElementById('name');
+const number = document.getElementById('number');
+const mail = document.getElementById('mai');
+const cards = document.querySelectorAll('.card');
+const genbtn = document.getElementById('gen');
+const genbtn1 = document.getElementById('gen1');
 
 custBtn.addEventListener('click', () => {
   if (custBtn.textContent.trim() === 'Search') {
+    const nameinp = inp1.value.trim();
+    const numinp = inp2.value.trim();
+
+    nameout.textContent = nameinp;
+    number.textContent = "Number: " + numinp;
+    mail.textContent = "Mail: " + nameinp + "@mail.ye"
     window1.classList.add('hiddenw');
     window2.classList.remove('hiddenw');
-    showToast('Quote searches the database (very quickly) and findes your customer!!');
+    showToast('Quote searches the database (very quickly) and findes your customer.');
   } else {
     showToast('That button is not a part of your expirence yet :(', 3000);
   }
+});
+
+cards.forEach(card => {
+  card.addEventListener('click', () => {
+    if (card.classList.contains('sel_q')) {
+      card.classList.remove('sel_q');
+      genbtn.classList.remove('sel');
+      genbtn1.classList.remove('sel');
+    } else {
+      cards.forEach(c => c.classList.remove('sel_q'));
+      card.classList.add('sel_q');
+      genbtn.classList.add('sel');
+      genbtn1.classList.add('sel');
+    }
+  });
 });
 
 function showToast(message, duration = 5000) {
@@ -75,3 +102,4 @@ function showToast(message, duration = 5000) {
     setTimeout(() => toast.remove(), 300);
   }, duration);
 }
+
