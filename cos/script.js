5f39c4fda393820ba3fb1fe5bf5818ff109f7255
ekah Pruthvi <p1sribhat@gmail.com>
2026-01-07 23:53:15 +0530

    cynageos hero one

--- cos/script.js
+++ cos/script.js
@@ -21,5 +21,26 @@ setTimeout(() => {
 }, 2000);
 
 setTimeout(() => {
-    typeWriter("These Entities govern them and they have rules, that have to be followed.", 50, "two");
+    typeWriter("These Entities govern them and they have rules that have to be followed.", 50, "two");
 }, 8000);
+
+setTimeout(() => {
+    typeWriter("Rules keep computers in check.", 50, "three");
+}, 15000);
+
+setTimeout(() => {
+    typeWriter("Rules ensure uniformity.", 50, "four");
+}, 20000);
+
+setTimeout(() => {
+    typeWriter("Rules makes smooth operations.", 50, "five");
+}, 24000);
+
+setTimeout(() => {
+    this.document.getElementById('one').style.visibility = 'hidden';
+    this.document.getElementById('two').style.visibility = 'hidden';
+    this.document.getElementById('three').style.visibility = 'hidden';
+    this.document.getElementById('four').style.visibility = 'hidden';
+    this.document.getElementById('five').style.visibility = 'hidden';
+    typeWriter("Rules Break", 50, "six");
+}, 29000);
